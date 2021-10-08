b2d706eaa79b6e150cb22172367fd5b3983f4d35
Anand <iamtheanandsingh@gmail.com>
2021-10-08 22:26:09 +0530

    08102021

---
+++ Flutter/apimplementation/android/app/src/main/kotlin/com/practice/api/apimplementation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.practice.api.apimplementation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/radradio/android/app/src/main/kotlin/com/nv/radradio/radradio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nv.radradio.radradio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
