64230f506dd12224f5b1c49f77ebe45317f25a92
Anand <iamtheanandsingh@gmail.com>
2021-10-19 22:45:27 +0530

    19102021

---
+++ Flutter/radradio/lib/pages/HomePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:radradio/utils/ai_util.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: AIColors.primaryColor1,
+    );
+  }
+}
