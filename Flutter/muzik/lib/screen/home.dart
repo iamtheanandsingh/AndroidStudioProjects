874c385cbe75f53990e7c806c9858f94e3983488
Anand Singh <iamtheanandsingh@gmail.com>
2021-11-19 18:33:36 +0530

    19112021

--- Flutter/muzik/lib/screen/home.dart
+++ Flutter/muzik/lib/screen/home.dart
@@ -10,6 +10,15 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      Container(
+        decoration: BoxDecration(
+          gradient: LinearGradient(
+              colors: [Colors.blueGrey.shade100,  Colors.black],
+            begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+stop: [0.1, 0.3]),
+      ),
+    );
   }
 }
